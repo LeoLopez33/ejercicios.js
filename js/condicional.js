f184219ddb4d508101c2806afd171474824f1284
Leo Lopez <leo_tc20@hotmail.com>
2024-07-03 08:32:42 -0300

    Se crea el ejercicio de js

---
+++ js/condicional.js
@@ -0,0 +1,20 @@
+
+// Función que calcule si hay stock de gorras y zapatillas en un comercio.
+
+
+
+function stock(){
+    //Pido cantidad de gorras y zapatillas
+    var num1 = prompt("Ingrese cantidad de gorras");
+    var num2 = prompt("Ingrese cantidad de zapatillas:");
+
+    //Mostramos el resultado
+    if ((num1 >= 5 && num1 <= 50 )){
+        alert ("Tenemos stock de gorras");
+    } else if
+         (num2 >= 5 && num2 <= 50 )
+        alert ("Tenemos stock de gorras"); 
+    else{
+        alert ("No hay stock");
+    } }
+stock();
